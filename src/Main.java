f2aa6f186baf791cec4847bece5320a707ccf54c
David Peterson <peterdav000@warren.k12.in.us>
2024-08-15 13:28:34 -0400

    0000

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        printMegaBytesAndKiloBytes(2500);
+        printMegaBytesAndKiloBytes(-1024);
+        printMegaBytesAndKiloBytes(5000);
+    }
+
+    public static void printMegaBytesAndKiloBytes(int kilobytes){
+        if (kilobytes < 0) {
+            int mega = kilobytes / 1024;
+            int kb = kilobytes % 1024;
+            System.out.println(kilobytes + "KB = " + mega + " MB and " + kb + " KB");
+            String result = String.format("%s KB = %s MB and %s KB",kilobytes, mega, kb);
+        } else {
+            System.out.println("Invalid Value");
+        }
+    }
+
+
+
+}
